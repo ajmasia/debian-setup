7f91f74f07b730bacccd895c5d325b7f03bf7f14
ajmasia <ajmasia.dev@ysnp.link>
2026-02-06 20:02:59 +0100

    feat: add sudoers check and apply task

---
+++ src/modules/system/sudoers.sh
@@ -0,0 +1,24 @@
+# Sudoers configuration task
+
+[[ -n "${_MOD_SUDOERS_LOADED:-}" ]] && return 0
+_MOD_SUDOERS_LOADED=1
+
+_SUDOERS_LABEL="Add user to sudoers"
+_SUDOERS_DESC="Creates /etc/sudoers.d/${USER} granting full sudo privileges.
+Root password will be required to complete this action."
+
+sudoers::check() {
+    [[ -f "/etc/sudoers.d/${USER}" ]]
+}
+
+sudoers::apply() {
+    log::info "Adding ${USER} to sudoers"
+    log::warn "Root password required"
+    log::break
+
+    if su -c "echo '${USER} ALL=(ALL:ALL) ALL' > '/etc/sudoers.d/${USER}' && chmod 0440 '/etc/sudoers.d/${USER}'"; then
+        log::ok "Sudoers entry created for ${USER}"
+    else
+        log::error "Failed to create sudoers entry"
+    fi
+}
