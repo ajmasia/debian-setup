87d09ac3003fa4c909cb7db9993a443eaef555f7
beta <beta@lab.link>
2026-02-06 18:46:21 +0100

    feat: add Catppuccin Mocha color palette
    Define ANSI escape and hex constants for the full Catppuccin Mocha
    palette, used across all CLI output and gum styling.

---
+++ src/lib/colors.sh
@@ -0,0 +1,68 @@
+# Catppuccin Mocha color palette
+# https://github.com/catppuccin/catppuccin
+
+[[ -n "${_LIB_COLORS_LOADED:-}" ]] && return 0
+_LIB_COLORS_LOADED=1
+
+# Base
+readonly COLOR_BASE=$'\033[38;2;30;30;46m'
+readonly COLOR_MANTLE=$'\033[38;2;24;24;37m'
+readonly COLOR_CRUST=$'\033[38;2;17;17;27m'
+readonly COLOR_SURFACE0=$'\033[38;2;49;50;68m'
+readonly COLOR_SURFACE1=$'\033[38;2;69;71;90m'
+readonly COLOR_SURFACE2=$'\033[38;2;88;91;112m'
+readonly COLOR_OVERLAY0=$'\033[38;2;108;112;134m'
+readonly COLOR_OVERLAY1=$'\033[38;2;127;132;156m'
+readonly COLOR_OVERLAY2=$'\033[38;2;147;153;178m'
+
+# Text
+readonly COLOR_TEXT=$'\033[38;2;205;214;244m'
+readonly COLOR_SUBTEXT0=$'\033[38;2;166;173;200m'
+readonly COLOR_SUBTEXT1=$'\033[38;2;186;194;222m'
+
+# Colors
+readonly COLOR_ROSEWATER=$'\033[38;2;245;224;220m'
+readonly COLOR_FLAMINGO=$'\033[38;2;242;205;205m'
+readonly COLOR_PINK=$'\033[38;2;245;194;231m'
+readonly COLOR_MAUVE=$'\033[38;2;203;166;247m'
+readonly COLOR_RED=$'\033[38;2;243;139;168m'
+readonly COLOR_MAROON=$'\033[38;2;235;160;172m'
+readonly COLOR_PEACH=$'\033[38;2;250;179;135m'
+readonly COLOR_YELLOW=$'\033[38;2;249;226;175m'
+readonly COLOR_GREEN=$'\033[38;2;166;227;161m'
+readonly COLOR_TEAL=$'\033[38;2;148;226;213m'
+readonly COLOR_SKY=$'\033[38;2;137;220;235m'
+readonly COLOR_SAPPHIRE=$'\033[38;2;116;199;236m'
+readonly COLOR_BLUE=$'\033[38;2;137;180;250m'
+readonly COLOR_LAVENDER=$'\033[38;2;180;190;254m'
+
+# Reset
+readonly COLOR_RESET=$'\033[0m'
+
+# Hex values (for gum and other tools that accept hex)
+readonly HEX_BASE="#1e1e2e"
+readonly HEX_MANTLE="#181825"
+readonly HEX_CRUST="#11111b"
+readonly HEX_SURFACE0="#313244"
+readonly HEX_SURFACE1="#45475a"
+readonly HEX_SURFACE2="#585b70"
+readonly HEX_OVERLAY0="#6c7086"
+readonly HEX_OVERLAY1="#7f849c"
+readonly HEX_OVERLAY2="#9399b2"
+readonly HEX_TEXT="#cdd6f4"
+readonly HEX_SUBTEXT0="#a6adc8"
+readonly HEX_SUBTEXT1="#bac2de"
+readonly HEX_ROSEWATER="#f5e0dc"
+readonly HEX_FLAMINGO="#f2cdcd"
+readonly HEX_PINK="#f5c2e7"
+readonly HEX_MAUVE="#cba6f7"
+readonly HEX_RED="#f38ba8"
+readonly HEX_MAROON="#eba0ac"
+readonly HEX_PEACH="#fab387"
+readonly HEX_YELLOW="#f9e2af"
+readonly HEX_GREEN="#a6e3a1"
+readonly HEX_TEAL="#94e2d5"
+readonly HEX_SKY="#89dceb"
+readonly HEX_SAPPHIRE="#74c7ec"
+readonly HEX_BLUE="#89b4fa"
+readonly HEX_LAVENDER="#b4befe"
