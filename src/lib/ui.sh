fef9c7a74c067ce7e73ee6e945b16eb2835fa525
Antonio Masiá <ajmasia.dev@ysnp.link>
2026-04-15 08:38:02 +0200

    feat: export distro id and show os info at startup

--- src/lib/ui.sh
+++ src/lib/ui.sh
@@ -122,7 +122,6 @@ ui::session_info() {
     _UI_SESSION_LOG_START="$(wc -l < "$log_file")"
 
     log::info "${date_str} - Session started by ${user}@${hostname} running ${os_name}"
-    log::break
 }
 
 ui::goodbye() {
